d78572fd0be903cd008cc6e60e73b98a031bd660
Biplob Hasan <fbiplobhasan2024@gmail.com>
2025-11-02 07:24:14 +0600

    practice js

--- module2.js
+++ module2.js
@@ -1,6 +1,38 @@
-const obj = {
-    nextLevel: { courseId: "Level2" },
-    "Programming Hero": { courseId: "Level1" }
-};
+// 01
+// const obj = {
+//     nextLevel: { courseId: "Level2" },
+//     "Programming Hero": { courseId: "Level1" }
+// };
 
-console.log(obj);
+// console.log(obj);
+// console.log(obj"nextLevel");
+// console.log(obj"Programming Hero");
+
+// 02
+// const obj = {
+//     nextLevel: { courseId: "Level2" },
+//     true: { courseId: "Level1" }
+// };
+
+// console.log(obj["true"]);
+
+// 03
+// const obj = {};
+// obj.nextLevel = { courseId: "Level2" };
+// obj[true] = { courseId: "Level1" };
+
+// console.log(obj);
+
+// 04
+// const course1 = {name: "Programming Hero"};
+// const course2 = {name: "Next Level Web Development"};
+
+// const obj = {};
+
+// obj[course2] = { courseId: "Level2" };
+// obj[course1] = { courseId: "Level1" };
+
+// console.log(obj["[object Object]"]);
+// console.log(obj["[object Object]"]);
+
+// 05
